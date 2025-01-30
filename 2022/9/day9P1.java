bbc56dbdacd7a1cfd96395671948d1f333f40a1f
kelvin <kelvinjyoga@gmail.com>
2025-01-30 14:33:56 +1100

    save point

--- 2022/9/day9P1.java
+++ 2022/9/day9P1.java
@@ -1,4 +1,3 @@
-// day 9 part2
 
 import java.io.IOException;
 import java.nio.file.*;
@@ -8,21 +7,11 @@ import java.util.HashSet;
 import java.util.stream.Stream;
 import java.util.List;
 
-public class day9 {
+public class day9P1 {
     public static void main(String[] args) {
         try(Stream<String> lines = Files.lines(Paths.get("input.txt"))) {
-            int[][] nodePositions = {
-                {0, 0},     // (0) head
-                {0, 0},     // (1) node 1
-                {0, 0},     // (2) node 2
-                {0, 0},     // (3) node 3
-                {0, 0},     // (4) node 4
-                {0, 0},     // (5) node 5
-                {0, 0},     // (6) node 6
-                {0, 0},     // (7) node 7
-                {0, 0},     // (8) node 8
-                {0, 0}      // (9) tail
-            };
+            int[] headPosition = {0, 0};
+            int[] tailPosition = {0, 0};
             HashSet<List<Integer>> visited = new HashSet<>(); // store all the visited places
 
             visited.add(Arrays.asList(0, 0));
@@ -30,7 +19,6 @@ public class day9 {
                 String[] movement = line.split("\\s+");
                 int amountOfMovement = Integer.parseInt(movement[1]);
 
-                // movement direction
                 int[] move = new int[2];
                 if(movement[0].equals("R")) {
                     move[0] = 0;
@@ -49,21 +37,11 @@ public class day9 {
                     move[1] = 0;
                 }
 
-                // move said times
                 for(int i = 0; i < amountOfMovement; i++) {
-                    // move the nodes in pairs of two
-                    // e.g. head, and node1 - node1 and node2 - etc. 'till tail
-                    print(nodePositions);
-                    for(int k = 0; k < 9; k++) {
-                        int[] tail = updatePosition(nodePositions[k], nodePositions[k + 1], move, k);
-                        if(k == 8) {
-                            visited.add(Arrays.asList(tail[0], tail[1]));
-                        }
-                    }
-                }  
+                    updatePosition(headPosition, tailPosition, move, visited);
+                }
             }
-            // System.out.println(visited);
-            System.out.println("Part2: " + visited.size());
+            System.out.println("Part1: " + visited.size());
 
         }
         catch(IOException e) {
@@ -72,29 +50,20 @@ public class day9 {
         }
     }
 
-    // print all the positions of the nodes
-    static void print(int[][] nodePositions) {
-        for(int[] i: nodePositions) {
-            System.out.println(i[0] + "," + i[1]);
-        }
-        System.out.println();
-    }
-
-    static int[] updatePosition(int[] headPosition, int[] tailPosition, int[] movement, int k) {
-        if(k == 0) {    // k == 0 means the head is being updated, if not then it's just the rest of the body
-            // new head position
-            headPosition[0] += movement[0];
-            headPosition[1] += movement[1];
-        }
+    static void updatePosition(int[] headPosition, int[] tailPosition, int[] movement, HashSet<List<Integer>> visited) {
+        // new head position
+        headPosition[0] += movement[0];
+        headPosition[1] += movement[1];
 
         if(adjacent(headPosition, tailPosition)) {    // check if they're still adjacent
-            return tailPosition;
+            return;
         }
 
         if(headPosition[0] == tailPosition[0] || headPosition[1] == tailPosition[1]) {  // if they're not diagonal
             tailPosition[0] += movement[0];
             tailPosition[1] += movement[1];
-            return tailPosition;
+            visited.add(Arrays.asList(tailPosition[0], tailPosition[1]));
+            return;
         }
 
         if(Math.abs(headPosition[0] - tailPosition[0]) > 1) {
@@ -115,7 +84,7 @@ public class day9 {
                 tailPosition[0] += 1;
             }
         }
-        return tailPosition;
+        visited.add(Arrays.asList(tailPosition[0], tailPosition[1]));
     }
 
     static boolean adjacent(int[] headPosition, int[] tailPosition) {
